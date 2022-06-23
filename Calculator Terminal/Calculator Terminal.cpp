611f2788fe351938d1d347ee88957e8d3e38ab0c
Dan F <danfdev33@gmail.com>
2022-06-23 15:58:10 -0400

    Added trigonometric functions feature and exponent mode feature

--- Calculator Terminal/Calculator Terminal.cpp
+++ Calculator Terminal/Calculator Terminal.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <math.h>
 #include "calculator.h"
 #include "rational.h"
 #include "calculator.h"
@@ -7,15 +8,16 @@ using namespace std;
 
 int main()
 {
-	char operMain, operA, operB;
+	char operMain, operA, operB, operD;
 	calculator calc1, calc2;
 	rational rat1, rat2;
-	cout << "Version: 1.0.1		Developed by: Mettagoner" << endl << endl;
+	cout << "Version: 1.1.1		Developed by: Mettagoner" << endl << endl;
 	do
 	{
 	clearMain:;
-		cout << "Press (a) for normal calculation   (b) for fraction calculation   (c) for square root calculation   (q)uit: ";
-		cin >> operMain; if ((operMain != 'a') && (operMain != 'b') && (operMain != 'c') && (operMain != 'q')) {std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); cout << "Illegal input. Resetting..." << endl; goto clearMain; }
+		cout << "Press (a) for normal calculation   (b) for fraction calculation   (c) for square root calculation" << endl; 
+		cout << "(d) for trigonometric functions   (e) for exponent mode   (q)uit:" << endl;
+		cout << "Input: ";  cin >> operMain; if ((operMain != 'a') && (operMain != 'b') && (operMain != 'c') && (operMain != 'd') && (operMain != 'e') && (operMain != 'q')) { std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); cout << "Illegal input. Resetting..." << endl; goto clearMain; }
 		switch (operMain)
 		{
 		case 'a':
@@ -23,7 +25,7 @@ int main()
 			cout << "NORMAL CALCULATION MODE ENABLED" << endl;
 		clearA:; cout << "Instructions:	[Enter num1 (+,-,*,/) num2]" << endl;
 			cout << "Input: ";
-			cin >> calc1; if (cin.fail()) {std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); cout << "Illegal input. Resetting..." << endl; goto clearA;}
+			cin >> calc1; if (cin.fail()) { std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); cout << "Illegal input. Resetting..." << endl; goto clearA; }
 			do {
 				cin >> operA; if ((operA != '+') && (operA != '-') && (operA != '*') && (operA != '/') && (operA != 'q') || (cin.fail())) { std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); cout << "Illegal input. Resetting..." << endl; goto clearA; }
 				//CLEAR OPERANDS
@@ -66,8 +68,8 @@ int main()
 
 		case 'b':
 			cout << endl << endl;
-		clearB:; cout << "FRACTION CALCULATION MODE ENABLED" << endl;
-			cout << "Instructions:   [num1/den1 (+,-,*,/) num2/den2]" << endl;
+			cout << "FRACTION CALCULATION MODE ENABLED" << endl;
+		clearB:; cout << "Instructions:   [num1/den1 (+,-,*,/) num2/den2]" << endl;
 			cout << "Input: ";
 			cin >> rat1; if (cin.fail()) { std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); cout << "Illegal input. Resetting..." << endl; goto clearB; }
 			do {
@@ -114,8 +116,8 @@ int main()
 			float sqroot;
 			char operSqroot;
 			cout << endl << endl; //format spacing
-		retryC:; cout << "SQUARE ROOT MODE ENABLED" << endl;
-			cout << "Instructions:   [Enter a value to square root]" << endl;
+			cout << "SQUARE ROOT MODE ENABLED" << endl;
+		retryC:; cout << "Instructions:   [Enter a value to square root]" << endl;
 			do
 			{
 				cout << "Input: ";
@@ -131,6 +133,109 @@ int main()
 			cout << "SQUARE ROOT MODE DISABLED" << endl;
 			cout << endl << endl; //format spacing
 			break; //break 'c' switch case
+
+		case 'd':
+			float inputD;
+			float aTan;
+			cout << endl << endl; //format spacing
+			cout << "TRIGONOMETRIC FUNCTION MODE ENABLED" << endl;
+		retryD:; cout << "NOTE: all functions take input in radians NOT degrees" << endl;
+			do {
+				cout << "Enter:" << endl;
+				cout << "(a) for SINE" << endl;
+				cout << "(b) for COSINE" << endl;
+				cout << "(c) for TANGENT" << endl;
+				cout << "(d) for ARC SINE" << endl;
+				cout << "(e) for ARC COSINE" << endl;
+				cout << "(f) for ARC TANGENT" << endl;
+				cout << "(g) for ARC TANGENT of a/b" << endl;
+				cout << "(h) for HYPERBOLIC COSINE" << endl;
+				cout << "(i) for HYPERBOLIC TANGENT" << endl;
+				cout << "(q) to return to root menu" << endl;
+				cout << "Input: ";  cin >> operD; if ((operD != 'a') && (operD != 'b') && (operD != 'c') && (operD != 'd') && (operD != 'e') && (operD != 'f') && (operD != 'g') && (operD != 'h') && (operD != 'i') && (operD != 'q')) { cout << "Illegal input. Resetting..." << endl; goto retryD; }
+				//QUIT TO ROOT
+				if (operD == 'q')
+				{
+					cout << "TRIGONOMETRIC FUNCTION MODE DISABLED" << endl;
+					cout << endl << endl; //format spacing
+					break;
+				}
+				//BEGIN SWITCH STATEMENT
+				switch (operD)
+				{
+				case 'a':
+					cout << "Enter value: ";
+					cin >> inputD; if (cin.fail()) { std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); cout << "Illegal input. Resetting..." << endl; goto retryD; }
+					cout << "SINE value of " << inputD << " = " << sin(inputD) << endl << endl;
+					continue;
+				case 'b':
+					cout << "Enter value: ";
+					cin >> inputD; if (cin.fail()) { std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); cout << "Illegal input. Resetting..." << endl; goto retryD; }
+					cout << "COSINE value of " << inputD << " = " << cos(inputD) << endl << endl;
+					continue;
+				case 'c':
+					cout << "Enter value: ";
+					cin >> inputD; if (cin.fail()) { std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); cout << "Illegal input. Resetting..." << endl; goto retryD; }
+					cout << "TANGENT value of " << inputD << " = " << tan(inputD) << endl << endl;
+					continue;
+				case 'd':
+					cout << "Argument must be in the range -1 to 1 otherwise an ERROR will occur" << endl;
+					cout << "Enter value: ";
+					cin >> inputD; if (cin.fail()) { std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); cout << "Illegal input. Resetting..." << endl; goto retryD; }
+					cout << "ARC SINE value of " << inputD << " = " << asin(inputD) << endl << endl;
+					continue;
+				case 'e':
+					cout << "Argument must be in the range -1 to 1 otherwise an ERROR will occur" << endl;
+					cout << "Enter value: ";
+					cin >> inputD; if (cin.fail()) { std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); cout << "Illegal input. Resetting..." << endl; goto retryD; }
+					cout << "ARC COSINE value of " << inputD << " = " << acos(inputD) << endl << endl;
+					continue;
+				case 'f':
+					cout << "Enter value: ";
+					cin >> inputD; if (cin.fail()) { std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); cout << "Illegal input. Resetting..." << endl; goto retryD; }
+					cout << "ARC TANGENT value of " << inputD << " = " << atan(inputD) << endl << endl;
+					continue;
+				case 'g':
+					cout << "Enter two values: ";
+					cin >> inputD; cin >> aTan; if (cin.fail()) { std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); cout << "Illegal input. Resetting..." << endl; goto retryD; }
+					cout << "ARC TANGENT value of " << inputD << " and " << aTan << " = " << atan2(inputD, aTan) << endl << endl;
+					continue;
+				case 'h':
+					cout << "Enter value: ";
+					cin >> inputD; if (cin.fail()) { std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); cout << "Illegal input. Resetting..." << endl; goto retryD; }
+					cout << "HYPERBOLIC COSINE of " << inputD << " = " << cosh(inputD) << endl << endl;
+					continue;
+				case 'i':
+					cout << "Enter value: ";
+					cin >> inputD; if (cin.fail()) { std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); cout << "Illegal input. Resetting..." << endl; goto retryD; }
+					cout << "HYPERBOLIC TANGENT of " << inputD << " = " << tanh(inputD) << endl << endl;
+					continue;
+				}
+			} while (operD != 'q');
+			break; //break 'd' switch case
+
+		case 'e':
+			char operE;
+			float numE, expE;
+			cout << endl << endl;
+			cout << "EXPONENT MODE ENABLED" << endl;
+		retryE:; cout << "Instructions:	[Enter a number then an exponent]" << endl;
+			do {
+				cout << "Enter a number: ";
+				cin >> numE; if (cin.fail()) { std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); cout << "Illegal input. Resetting..." << endl; goto retryE; }
+				cout << "Enter an exponent: ";
+				cin >> expE; if (cin.fail()) { std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); cout << "Illegal input. Resetting..." << endl; goto retryE; }
+				cout << "Result: " << pow(numE, expE) << endl;
+				cout << "[Press (r) to retry] or [Press (q) to quit to root]: ";
+				cin >> operE; if ((operE != 'r') && (operE != 'q')) { std::cin.clear(); std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); cout << "Illegal input. Resetting..." << endl; goto retryE; }
+				if (operSqroot == 'r')
+				{
+					goto retryC;
+				}
+			} while (operE != 'q');
+			cout << "EXPONENT MODE DISABLED" << endl;
+			cout << endl << endl; //format spacing
+			break; //case 'e' switch case
 		}//break operMain switch case
 	} while (operMain != 'q');
 	return 0;
